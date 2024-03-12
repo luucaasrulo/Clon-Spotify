926ef45be707fe50efe8831a81bfa0f058647e53
luucaasrulo <lucas___1997@hotmail.com>
2024-03-12 11:39:40 -0300

    player

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
+  output: 'server',
 });

5f629071881590da4d1a93f35c0f8ea8bd707fcb
luucaasrulo <lucas___1997@hotmail.com>
2024-03-13 07:30:39 -0300

    Correcciones de color

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -25,7 +25,7 @@ const CurrentSong = ({ image, title, artists }) => {
         flex items-center gap-5 relative
         overflow-hidden
       `}>
-        <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+        <picture className="w-16 h-16 rounded-md shadow-lg overflow-hidden">
           <img src={image} alt={title} />
         </picture>
 
